82e7c9e91a675df65ff463946e92ae5d99696bea
Daniil <dotagum687@gmail.com>
2026-03-24 10:44:57 +0300

    День 2: - Видеоурок -> 5

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	todo_app "github.com/Daniil-1622/todo-app"
 	"github.com/Daniil-1622/todo-app/pkg/handler"
 	"github.com/Daniil-1622/todo-app/pkg/repository"
 	"github.com/Daniil-1622/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -14,12 +17,29 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("init config err: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("load .env file err: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to init db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
-	srv := new(todo_app.Server)                                                 // Создаем новый экземпляр структуры Server
-	if err := srv.Run(viper.GetString(""), handlers.InitRoutes()); err != nil { // Запускаемся на порту 8000
+	srv := new(todo_app.Server)                                                     // Создаем новый экземпляр структуры Server
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil { // Запускаемся на порту 8000
 		// Если возникает ошибка, выводим ее
 		log.Fatalf("error occured while running server: %s", err.Error())
 	}
